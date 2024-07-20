938a8b40a4bbd93c28af946c37b44fca319b9d2c
Brendan Wadey <62085968+Broondoon@users.noreply.github.com>
2024-07-20 17:28:40 -0600

    Added info to the readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # campus-cruisers
 ECE 470 Group Project
+
+
+
+## Design Specifications
+- dataType.py
+ - Contains the custom data objects we will be using.
+ - Solution class
+  - Data: a list of Nodes, optionally a fitness value
+  - Methods: tbd
+ - Node class
+  - Data: id and coord
+  - Methods: get_neighbours(Node), tbd
+ - Lookup_table class
+  - Data: nxn matrix of arc lengths between nodes
+  - Methods: get_node(id), tbd
+- fitness.py
+ - Methods: get_fitness(Solution), check_termination(generation)
+- combine_solns.py
+ - Methods: select_mutation(Solution), mutate(Solution, Node_curr, Node_mut), select_crossover(Solution), crossover(Solution, Node_cross_point)
+
---
+++ src/util/combine_solns.py
@@ -0,0 +1,56 @@
+"""
+combine_solns.py
+
+Contains methods for crossover and mutation.
+
+"""
+import random
+
+dummy_lookup_table = [0, 0][0, 0]
+
+class Solution():
+    nodes = []
+
+    def __init__(self, nodes):
+        self.nodes = nodes
+
+def select_mutation(soln):
+    nodes = soln.nodes
+    could_mutate = []
+    # Range is funky so as to skip mutating the first and last nodes
+    for i in range(1, len(nodes) - 1):
+        i_neighbours = nodes[i].get_neighbours()
+
+        options = []
+        for neighbour in i_neighbours:
+            if nodes[i-1] in neighbour.get_neighbours() and nodes[i+1] in neighbour.get_neighbours():
+                options.append(neighbour)
+
+        if len(options) > 0:
+            could_mutate.append((i, options))
+        
+    # Randomly choose which valid node to mutate
+    node_to_mutate = random.randrange(0, len(could_mutate))
+    index = node_to_mutate[0]
+
+    # Randomly choose which mutation to take
+    replacement = node_to_mutate[1][random.randrange(0, len(node_to_mutate[1]))]
+
+    return(index, replacement)
+
+
+# TODO: add this to solution class
+def mutate(self, ):
+    
+    soln.replace_node(index, replacement)
+        
+
+soln = Solution()
+select_mutation(soln)
+        
+
+
+    
+    
+        
+
