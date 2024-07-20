5b11c1d2cd78abf19623ee9c27f9f0b7c626cb9a
Fawzan Hussain <fawzan.fsl@gmail.com>
2024-07-20 16:00:58 -0700

    Datatype.py has been added.

---
+++ src/util/dataType.py
@@ -0,0 +1,73 @@
+"""
+dataType.py
+
+Contains the definition for the node() object class.
+
+We could also update this file to have any/all custom objects/classes.
+
+"""
+import numpy as np
+
+def defineNode:
+    CLE = {0,48.46385902698279, -123.31022982698542}
+    DTB = {1, 48.465078386216554, -123.31387649906908}
+    A = {2, 48.46113617040822, -123.31144840329222}
+    B = {3,}
+    C ={4,}
+    D = {5, }
+    E = {6, }
+    F = {7, }
+    G = {8, }
+    H = {9, }
+    I = {10, }
+    J = {11, }
+    K = {12, }
+    L = {13, }
+    M = {14, }
+    N = {15, }
+    O = {16, }
+    P = {17, }
+    Q = {18, }
+    R = {19, }
+    S = {20, }
+    T = {21, }
+    U = {22, }
+    V = {23, }
+    W = {24, }
+    X = {25, }
+    Y = {26, }
+    Z = {27, }
+    AA =
+    AB =
+    AC =
+    AD =
+    AE =
+    AF =
+    AG =
+    AH =
+    AI =
+    AJ =
+    AK =
+    AL =
+    AM =
+    AN =
+    AO =
+    AP =
+    AQ =
+    AR =
+    AS =
+    AT =
+    AU =
+    AV =
+    AW =
+    AX =
+    AY =
+    AZ =
+    BA =
+    BB =
+    BC =
+    BD =
+    BE =
+    BF =
+    BG =
+    BH =
--- src/util/node.py
+++
@@ -1,9 +0,0 @@
-"""
-node.py
-
-Contains the definition for the node() object class.
-
-We could also update this file to have any/all custom objects/classes.
-
-"""
-import numpy as np
