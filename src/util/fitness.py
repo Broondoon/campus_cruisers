724cc8e7336dc3f1dccb25f4d3d81a3f570594cc
Brendan Wadey <62085968+Broondoon@users.noreply.github.com>
2024-07-22 13:13:13 -0600

    check_terminate plan for fitness.py
    Worked with Bear to plan function contract

--- src/util/fitness.py
+++ src/util/fitness.py
@@ -19,3 +19,21 @@ def fitness(potential_solution, lookup_table):
     answer = total_distance
     return answer
 
+"""
+Scratchwork:
+
+terminate on 5 rounds of no change (stagnation limit)
+max deviation = 2m (max_dev)
+
+[82m, 83m, 84m, 82m, 83m] <--- best fitness from previous gen
+standard deviation? There's definitely a math.py function to quick calc
+"""
+
+# Input: list of solutions (curr_generation), fitness history, max rounds of no change before term, max deviation
+# Ex: check_terminate([92m, 83m, 88m, 86m], ,82m, 83m, 84m, 82m, 83m], 5, 2)
+# Output: bool
+def check_terminate(curr_generation, fitness_history, stagnation_limit, max_dev):
+    pass
+
+    # best_soln = curr_generation.max <-- there's definitely a python function to grab biggest #
+
