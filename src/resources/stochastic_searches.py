c6c6c86ea60f744c36375a8afcdfd6099fe18545
Bear <40529665+BearSpry@users.noreply.github.com>
2024-07-29 15:07:05 -0700

    Create outline for function. Creates the outline of the funtion with the start of implementation and comments describing the yet to be complete sections.

--- src/resources/stochastic_searches.py
+++ src/resources/stochastic_searches.py
@@ -7,5 +7,22 @@ Bear!
 
 """
 
-def random_restart_hill_climbing():
-    pass
+
+# Based on the randomized depth first search described here:
+# https://en.wikipedia.org/wiki/Maze_generation_algorithm#Iterative_implementation_(with_stack)
+#
+def random_depth_first_search(starting_node, ending_node):
+    # begin at the starting node
+    node_list = [starting_node]
+    visited = [starting_node.id]
+    # main loop
+    while len(node_list) > 0:
+        # if node_list[0] == ending_node, exit loop
+        # get node_list[0] neighbours
+        # check if visited
+        # if unvisited, randomly pick one and add to start of node_list and visited
+        # if all visited, removed node_list[0] from node_list
+        break
+    # node_list contains a solution or is empty if ending_node not found
+    return node_list
+
